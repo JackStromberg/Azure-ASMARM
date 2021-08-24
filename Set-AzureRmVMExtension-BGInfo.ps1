e39e70d9c17099c41653b29ccf80c676a033b086
Jack Stromberg <jstrom@microsoft.com>
2021-08-24 12:34:50 -0500

    Add back BG Info
    Add back BG Info

---
+++ Set-AzureRmVMExtension-BGInfo.ps1
@@ -0,0 +1,18 @@
+<#
+*
+* Author:	Jack Stromberg
+* Email:	jstrom@microsoft.com
+* Date:		8/24/2021
+* Version:  1.0
+*
+* Changelog
+* v1.0 - Last Modified: 8/24/2021
+*
+* Notes:
+*       This is an example of installing a VM extension (BGinfo used as an example) post ASM -> ARM migration.
+*
+* Assumptions:
+*       This is a manual installation of BGInfo extension. The following cmdlet could be used as well:
+*       https://docs.microsoft.com/en-us/powershell/module/az.compute/set-azvmbginfoextension?view=azps-6.3.0
+#>
+Set-AzureRmVMExtension -ExtensionName BGInfo -Publisher Microsoft.Compute -Version 2.1 -ExtensionType BGInfo -Location westus -ResourceGroupName YOUR_RESOURCE_GROUP -VMName YOUR_VM_NAME
