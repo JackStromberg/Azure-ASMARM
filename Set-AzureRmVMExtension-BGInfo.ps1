82334401b55d05270abb2d0ae9e7cb9a5071a1cc
Jack Stromberg <jstrom@microsoft.com>
2021-08-26 11:31:10 -0500

    Changing command to reflect latest Az cmdlets
    Changing command to reflect latest Az cmdlets

--- Set-AzureRmVMExtension-BGInfo.ps1
+++ Set-AzureRmVMExtension-BGInfo.ps1
@@ -1,12 +1,13 @@
 <#
 *
-* Author:	Jack Stromberg
-* Email:	jstrom@microsoft.com
-* Date:		8/24/2021
-* Version:  1.0
+* Author:		Jack Stromberg
+* Email:		jstrom@microsoft.com
+* Date:		  8/24/2021
+* Version:	1.0
 *
 * Changelog
-* v1.0 - Last Modified: 8/24/2021
+* v1.0 - Initial Release
+* v1.1 - Swapping ARM command: 8/26/2021
 *
 * Notes:
 *       This is an example of installing a VM extension (BGinfo used as an example) post ASM -> ARM migration.
@@ -15,4 +16,4 @@
 *       This is a manual installation of BGInfo extension. The following cmdlet could be used as well:
 *       https://docs.microsoft.com/en-us/powershell/module/az.compute/set-azvmbginfoextension?view=azps-6.3.0
 #>
-Set-AzureRmVMExtension -ExtensionName BGInfo -Publisher Microsoft.Compute -Version 2.1 -ExtensionType BGInfo -Location westus -ResourceGroupName YOUR_RESOURCE_GROUP -VMName YOUR_VM_NAME
+Set-AzVMExtension -ExtensionName BGInfo -Publisher Microsoft.Compute -Version 2.1 -ExtensionType BGInfo -Location westus -ResourceGroupName YOUR_RESOURCE_GROUP -VMName YOUR_VM_NAME
